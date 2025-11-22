9cbbbf6e61332d57322d1e90c52b4be7c7fb97ce
David Idowu <idowudavidodun@gmail.com>
2025-11-22 14:27:12 +0100

    added: error message for frontend signup

--- Todo-Dashboard/frontend/src/features/auth/SignupPage.tsx
+++ Todo-Dashboard/frontend/src/features/auth/SignupPage.tsx
@@ -3,17 +3,29 @@ import { Link, useNavigate } from "react-router-dom";
 // import { useAppDispatch } from "../../hooks";
 // import { loginSuccess } from "./AuthSlice";
 import axios from "axios";
+import SignupValidation from "./Validation";
 
 const LoginPage = () => {
-  // const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
+    const validationErrors = SignupValidation({ username, email, password });
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length > 0) {
+      return;
+    }
+
     try {
       const response = await axios.post("http://localhost:5000/auth/signup", {
         password,
@@ -31,46 +43,48 @@ const LoginPage = () => {
   const inputStyle =
     "border border-b-2 border-x-0 border-t-0 py-2 w-full shadow-md pl-2 focus:shadow-xl focus:outline-none focus:p-3 rounded-md focus:font-semibold font-semibold focus:text-md";
 
+  const errorMessageStyle = "text-red-500 font-semibold text-sm ml-3";
+
   return (
     <div className="w-full bg-blue-500 h-dvh bg-[url('/login-background.png')] bg-no-repeat bg-cover flex flex-col justify-center items-center">
       <div className="bg-white w-[80%] h-[85%] p-6 pt-10 rounded-3xl relative max-w-sm max-h-[610px]">
-        <div className="relative z-20 ">
+        <div className="">
           <h2 className="font-bold text-[2.15rem]">Sign up</h2>
           <p className="font-semibold text-lg text-gray-500">
             Please sign up to continue
           </p>
-          <form onSubmit={handleSubmit} className="py-5 gap-3 flex flex-col">
-            <input
-              placeholder="USERNAME"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className={inputStyle}
-              required
-              minLength={6}
-              maxLength={20}
-            />
+          <form onSubmit={handleSubmit} className="py-5 gap-1 flex flex-col">
+            <div className="flex flex-col">
+              <input
+                placeholder="USERNAME"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className={inputStyle}
+              />
+              {<span className={errorMessageStyle}>{errors.username}</span>}
+            </div>
+
             <input
               placeholder="EMAIL"
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className={inputStyle}
-              required
             />
+            {<span className={errorMessageStyle}>{errors.email}</span>}
             <input
               placeholder="PASSWORD"
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className={inputStyle}
-              required
-              minLength={4}
+              minLength={6}
             />
+            {<span className={errorMessageStyle}>{errors.password}</span>}
             <button
               type="submit"
               className="p-3 bg-blue-500 text-white rounded-full shadow-md w-32 mt-5 hover:bg-slate-800"
-              onClick={() => console.log("Clicked")}
             >
               Sign up
             </button>
--- Todo-Dashboard/frontend/src/features/auth/Validation.ts
+++ Todo-Dashboard/frontend/src/features/auth/Validation.ts
@@ -5,27 +5,32 @@ interface ValidationProps {
 }
 
 const SignupValidation = (values: ValidationProps) => {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+
   let error = {
     username: "",
     email: "",
     password: "",
   };
 
-  if (values.username === "") {
+  if (values.username.trim() === "") {
     error.username = "Username cannot be empty";
-  }
+  } else if (values.username.length < 3) {
+		error.username = "Username is too short"
+	}
 
-  if (values.email === "") {
+  if (values.email.trim() === "") {
     error.email = "Email cannot be empty";
+  } else if (!emailPattern.test(values.email)) {
+    error.email = "Invalid email format.";
   }
 
   // validate password with the db
   if (values.password.length === 0) {
     error.password = "Password cannot be empty";
-  } else if (values.password.length < 3) {
+  } else if (values.password.length < 4) {
     error.password = "Password is too short";
-  }
-
+	}
   return error;
 };
 
